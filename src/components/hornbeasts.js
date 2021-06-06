d800f488d09ad10c9182f50fd713156bf216b665
Nadeen-Shuweihat <shwueihat16@gmail.com>
2021-06-06 17:21:22 +0300

    vreated the basic

---
+++ src/components/hornbeasts.js
@@ -0,0 +1,11 @@
+import React from 'react';
+class HornBeasts extends React.Component {
+    render() {
+        return ( <
+            div >
+            <
+            h2 > { this.props.title } < /h2> <
+            /div>
+        )
+    }
+}
---
+++ src/components/main.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import HornBeasts from './hornbeasts';
+
+class Main extends React.Component {
+
+    render() {
+
+
+        return ( <
+            main >
+            <
+            hornbeasts title = 'UniWhal' > < /hornbeasts> <
+            /main>
+        )
+    }
+}
+
+export default Main;
